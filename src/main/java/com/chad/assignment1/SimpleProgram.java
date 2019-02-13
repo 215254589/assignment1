9f7f0721e51d202c8273ac230270152972c4f101
21600WJK9529 <43211428+21600WJK9529@users.noreply.github.com>
2019-02-13 23:43:17 +0200

    Add files via upload
    Added scanner for input and some printlns to show whats happening

--- src/main/java/com/chad/assignment1/SimpleProgram.java
+++ src/main/java/com/chad/assignment1/SimpleProgram.java
@@ -1,21 +1,28 @@
 package com.chad.assignment1;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Chad Boswell
  */
-public class SimpleProgram {
 
+public class SimpleProgram {
+static Scanner input=new Scanner(System.in);
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
 // TODO code application logic here
 
-        int     num1= 4,
-                num2= 7,
-                num3= 6;
-
+        int     num1,num2,num3;
+        System.out.println("Enter a value");
+        num1=input.nextInt();
+        System.out.println("Enter a 2nd value");
+        num2=input.nextInt();
+        System.out.println("Enter 3rd value");
+        num3=input.nextInt();
+                System.out.println("Largest number finder");
         if(num1> num2 && num1 > num3)
         {
             System.out.println(num1+ " is the largest number");
@@ -30,7 +37,7 @@ public class SimpleProgram {
         {
             System.out.println(num3+ " is the largest number");
         }
-
+            System.out.println("From the values:"+num1+","+num2+","+num3);
     }
 
 }
